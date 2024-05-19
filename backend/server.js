b4d3d56cf8e6cb788ab34a3d9310d20eb9a9aa24
Jeesun Bari <jeesunbari2002@gmail.com>
2024-05-19 19:49:00 +0530

    Add dependencies and configuration files for frontend development & create models

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,22 @@
 const express = require('express')
 const dotenv = require('dotenv')
+const cors = require("cors");
+const chats = require("./data/data.js");
 dotenv.config()
 const app = express();
 const PORT = process.env.PORT || 3000
 
+app.use(cors());
 
 app.get('/', (req, res) =>{
     res.send("Hello world, Chat API is running...")
 })
 
+app.get('/api/chat', (req, res) => {
+  res.send(chats);
+})
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
 })
+
