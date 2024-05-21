92ea35e4c37ac6e6def10c81103d4c3c20e7ba30
Jeesun Bari <jeesunbari2002@gmail.com>
2024-05-22 01:23:56 +0530

    Add user registration and login functionality

---
+++ backend/controllers/user.controllers.js
@@ -0,0 +1,60 @@
+const asyncHandler = require("express-async-handler");
+const User = require("../models/user.model");
+const generateToken = require("../config/generateToken");
+
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password, avatar } = req.body;
+
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error("Please Enter all the feilds");
+  }
+
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error("User Already Exists");
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+    avatar,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      avatar: user.avatar,
+      token: generateToken(user._id)
+    });
+  } else {
+    res.status(400);
+    throw new Error("Failed to create user");
+  }
+});
+
+const loginUser = asyncHandler(async (req, res) => {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if(user && (await user.matchPassword(password))){
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            avatar: user.avatar,
+            token: generateToken(user._id)
+        })
+    }else{
+        res.status(401);
+        throw new Error('Invalid Email or Password')
+    }
+})
+
+module.exports = { registerUser, loginUser };
